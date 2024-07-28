4c51319acad502c83c968282dd3251a96ef8decc
Ally Elvis Nzeyimana <155692151+allyelvis@users.noreply.github.com>
2024-07-28 16:48:15 +0000

    Update

--- facture_project/facture_project/urls.py
+++ facture_project/facture_project/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.urls import path, include
 from django.urls import path
 
 urlpatterns = [
+    path('facture/', include('facture.urls')),
+
     path('facture/', include('facture.urls')),
 
     path('admin/', admin.site.urls),
